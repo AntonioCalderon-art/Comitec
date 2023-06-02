9ae1b18e1fb0c2ce9cd2d3e094ddbc63c7148c80
Antonio Calderón - Trabajo <antonio.calderon@coppel.com>
2023-06-01 18:32:17 -0700

    V1.0.0.1
    Se creo la pantallla principal alumno con sus eventos en los registros, de igual manera la funcionalidad para mostrar las pantallas de los perfiles deseados.
    
    Aún no hay base de datos conectada.

--- ComiTec/assets/js/eventos.js
+++ ComiTec/assets/js/eventos.js
@@ -5,6 +5,11 @@ $(document).ready(function () {
     $("#CardPorFirmar").on("click", poneLblPorFirmar);
     $("#btnIngresar").on("click", iniciarSesion);
     $("#btnCerrarSesion").on("click", cerrarSesion);
+    $("#CardAutorizados").on("click", poneLblAutorizados);
+    $("#CardPendientes").on("click", poneLblPendientes);
+    $("#CardRechazados").on("click", poneLblRechazados);
+    $("#divNuevaSolicitud").on("click", redireccionNuevaSolicitud);
+    $("#btnEnviarSolicitud").on("click", enviarSolicitud);
 });
 
 function poneLblRevisados() {
@@ -23,10 +28,40 @@ function poneLblPorFirmar() {
     $("#lblTipoPeticion").html("Solicitudes por firmar");
 }
 
+function poneLblAutorizados() {
+    $("#lblTipoSolicitud").html("Solicitudes autorizadas");
+}
+
+function poneLblPendientes() {
+    $("#lblTipoSolicitud").html("Solicitudes pendientes");
+}
+
+function poneLblRechazados() {
+    $("#lblTipoSolicitud").html("Solicitudes rechazadas");
+}
+
+
 function iniciarSesion() {
-    location.href ='index.html';
+    var perfil = $("#cboPerfil").val();
+
+    if (perfil == "1") {
+        location.href = 'principal_alumno.html';
+    } else if (perfil == "2") {
+        location.href = 'index.html';
+    }else{
+        alert("Elige un perfil de usuario válido");
+    }
+}
+
+function redireccionNuevaSolicitud() {
+    location.href = 'agendar_cita.html';
 }
 
 function cerrarSesion() {
-    location.href ='login.html';
+    location.href = 'login.html';
+}
+
+function enviarSolicitud() {
+    alert("Solicitud enviada con exito");
+    location.href = 'principal_alumno.html';
 }
